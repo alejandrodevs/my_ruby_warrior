a5b5ba8ef7a86d01603784c3ec65b103b0245086
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-05-12 19:00:35 -0500

    Refactoring Beginner Level

--- rubywarrior/alejandrodevs-beginner/warrior.rb
+++ rubywarrior/alejandrodevs-beginner/warrior.rb
@@ -12,23 +12,16 @@ class Warrior
 
   def prepare warrior
     @warrior = warrior
-  end
-
-  def direction
-		@to = set_direction
+    @to = set_direction
   end
 
   def play!
-		if should_retire? then retire!
+    if should_retire? then retire!
     elsif should_rest? then rest!
     elsif feel.empty? then safe? ? walk! : should_shoot? ? shoot! : walk!
     elsif !feel.empty? then feel.captive? ? rescue! : attack!
     end
-  end
-
-  def memories
     @previous_health = warrior.health
   end
 
 end
-
