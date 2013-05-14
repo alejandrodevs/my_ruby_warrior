012f3f2ebc6e52cf7cfb564594fdd13edf45801f
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-05-14 18:20:38 -0500

    Refactoring should_shoot? method and adding should_shoot_shooters? and should_shoot_sludges? methods

--- rubywarrior/alejandrodevs-beginner/warrior_helpers/senses/shoulds.rb
+++ rubywarrior/alejandrodevs-beginner/warrior_helpers/senses/shoulds.rb
@@ -15,8 +15,7 @@ module WarriorHelpers
       end
 
       def should_shoot?
-        nothing?(0) && ((wizard?(1) || (nothing?(1) && shooter?(2))) ||
-        ((sludges?(1) ? @shoots <= (small_sludge?(1) ? 0 : 6) : false) && !there_archers?(:backward) ))
+        should_shoot_shooters? || should_shoot_sludges? && !there_archers?(:backward)
       end
 
       def should_walk?
@@ -29,6 +28,14 @@ module WarriorHelpers
 
       def should_rescue?
         feel.captive?
+      end
+
+      def should_shoot_shooters?
+        nothing?(0) && (wizard?(1) || (nothing?(1) && shooter?(2)))
+      end
+
+      def should_shoot_sludges?
+        nothing?(0) && sludges?(1) && (@shoots <= (small_sludge?(1) ? 0 : 6))
       end
 
     end
